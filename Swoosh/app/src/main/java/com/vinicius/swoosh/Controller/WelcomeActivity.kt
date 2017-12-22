5d22523ee1210229776db13485f5c1cc0bf94623
vinicius p vieira <vinipachecov@gmail.com>
2017-12-22 12:27:01 -0200

    package organization change, controller package, creation of finish layout, managing the last intent, sending information between all intents

--- Swoosh/app/src/main/java/com/vinicius/swoosh/Controller/WelcomeActivity.kt
+++ Swoosh/app/src/main/java/com/vinicius/swoosh/Controller/WelcomeActivity.kt
@@ -1,8 +1,8 @@
-package com.vinicius.swoosh
+package com.vinicius.swoosh.Controller
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.vinicius.swoosh.R
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : BaseActivity() {
---
+++ Swoosh/app/src/main/java/com/vinicius/swoosh/Utilities/constants.kt
@@ -0,0 +1,7 @@
+package com.vinicius.swoosh.Utilities
+
+/**
+ * Created by vinicius on 12/22/17.
+ */
+const val EXTRA_LEAGUE = "league"
+const val EXTRA_SKILL = "skill"
