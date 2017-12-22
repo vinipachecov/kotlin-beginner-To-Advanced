66a54871d5101c270907dc8aaac4a2a1b54ab27d
vinicius p vieira <vinipachecov@gmail.com>
2017-12-22 18:22:54 -0200

    second layout beginning

--- CoderSwag/app/src/main/java/com/vinicius/coderswag/Controller/MainActivity.kt
+++ CoderSwag/app/src/main/java/com/vinicius/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.vinicius.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.vinicius.coderswag.Model.Category
 import com.vinicius.coderswag.R
+import com.vinicius.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,  android.R.layout.simple_list_item_1,
+                DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
