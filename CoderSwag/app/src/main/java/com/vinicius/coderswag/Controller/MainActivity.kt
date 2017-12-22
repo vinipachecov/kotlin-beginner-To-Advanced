c879697ab9a64e72f978d09acda99dfab9ef0e51
vinicius p vieira <vinipachecov@gmail.com>
2017-12-22 19:04:30 -0200

    changed arrayAdapter to a custom categoryAdapter

--- CoderSwag/app/src/main/java/com/vinicius/coderswag/Controller/MainActivity.kt
+++ CoderSwag/app/src/main/java/com/vinicius/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.vinicius.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.vinicius.coderswag.Adapters.CategoryAdapter
 import com.vinicius.coderswag.Model.Category
 import com.vinicius.coderswag.R
 import com.vinicius.coderswag.Services.DataService
@@ -10,14 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,  android.R.layout.simple_list_item_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
