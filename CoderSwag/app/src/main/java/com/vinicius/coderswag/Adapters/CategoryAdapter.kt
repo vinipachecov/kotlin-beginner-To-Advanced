d2b703e42560410d296cf5d320953d35c073d36b
vinicius p vieira <vinipachecov@gmail.com>
2017-12-23 17:17:36 -0200

    recycler view with our custom adapter

--- CoderSwag/app/src/main/java/com/vinicius/coderswag/Adapters/CategoryAdapter.kt
+++ CoderSwag/app/src/main/java/com/vinicius/coderswag/Adapters/CategoryAdapter.kt
@@ -22,19 +22,32 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
+        val holder: ViewHolder
 
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item,null)
-        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
-        println("heavy computing")
+        if (convertView == null) {
+
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item,null)
+            holder = ViewHolder()
+
+//            get the reference to our imagesviews and textviews
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+//            now we can re use / recycle the references instead of loading every single movement
+            categoryView.tag = holder
+        } else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+
+        }
+
         val category = categories[position]
 
 
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryImage.setImageResource(resourceId)
-        println(resourceId)
+        holder.categoryImage?.setImageResource(resourceId)
 
-        categoryName.text = category.title
+        holder.categoryName?.text = category.title
         return categoryView
     }
 
@@ -50,4 +63,8 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
         return categories.count()
     }
 
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
+    }
 }
