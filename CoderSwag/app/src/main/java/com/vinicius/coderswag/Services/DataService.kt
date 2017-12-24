d75a897da49f3904e9a71a2cad26a98f155750c9
vinicius p vieira <vinipachecov@gmail.com>
2017-12-24 19:10:20 -0200

    fix

--- CoderSwag/app/src/main/java/com/vinicius/coderswag/Services/DataService.kt
+++ CoderSwag/app/src/main/java/com/vinicius/coderswag/Services/DataService.kt
@@ -15,6 +15,14 @@ object DataService {
             Category("SHIRTS", "shirtimage"),
             Category("HOODIES", "hoodieimage"),
             Category("HATS", "hatimage"),
+            Category("DIGITAL", "digitalgoodsimage"),
+            Category("SHIRTS", "shirtimage"),
+            Category("HOODIES", "hoodieimage"),
+            Category("HATS", "hatimage"),
+            Category("DIGITAL", "digitalgoodsimage"),
+            Category("SHIRTS", "shirtimage"),
+            Category("HOODIES", "hoodieimage"),
+            Category("HATS", "hatimage"),
             Category("DIGITAL", "digitalgoodsimage")
     )
 
